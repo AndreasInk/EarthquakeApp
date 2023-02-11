24592ed14f1835699b9a413ce66f036454beef6b
Andreas Ink <andreasink17@gmail.com>
2023-02-11 14:53:00 -0500

    Adds notifications

---
+++ EarthquakeApp/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  EarthquakeApp
+//
+//  Created by Andreas Ink on 2/10/23.
+//
+
+import SwiftUI
+
+struct AppDelegate: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppDelegate_Previews: PreviewProvider {
+    static var previews: some View {
+        AppDelegate()
+    }
+}
