e638c46bfa39b1fe53968dc07c13c7cba662ea90
Andreas Ink <andreasink17@gmail.com>
2023-02-10 22:53:01 -0500

    Initial Commit

---
+++ EarthquakeApp/EarthquakeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakeAppApp.swift
+//  EarthquakeApp
+//
+//  Created by Andreas Ink on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
